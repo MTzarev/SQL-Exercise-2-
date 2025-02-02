1d7fc1b5283098787427cc8441358b0786f86cb7
Miroslav Tsarev <mtzarev@gmail.com>
2025-02-02 21:16:15 +0200

    Update SQL Exercise new.sql

--- SQL Exercise new.sql
+++ SQL Exercise new.sql
@@ -1,4 +1,104 @@
+Create DataBase `minions`; 
+Use `minions`;
 
+CREATE TABLE `minions` (
+	`id` int primary key auto_increment, 
+    `name` Varchar (50) not null,
+    `age` int
+);
+Create Table `towns` (
+`town_id` INT Primary key Auto_increment, 
+`name` Varchar (30) not null
+);
+alter table `minions`
+ADD COLUMN `town_id`INT, 
+ADD Constraint fk_minions_towns
+FOREIGN KEY (`town_id`) 
+References `towns`(`id`);
+
+insert into `towns`
+values 
+(1, 'Sofia'),
+(2, 'Plovdiv'),
+(3, 'Varna');
+
+insert into `minions`
+values
+(1, 'Kevin', 22, 1),
+(2, 'Bob', 15, 3),
+(3, 'Steward', NULL, 2);
+
+truncate `minions`;
+DROP Table `minions`;
+Drop Table `towns`;
+
+select * from `minions`;
+truncate people;
+select * from people;
+
+-- EX 6:
+drop table people;
+drop table users;
+Drop Schema minions;
+ Create Table `people`(
+ `id` INT Primary key auto_increment,
+ `name` VARCHAR (200) NOT NULL,
+ `picture` BLOB,
+ `height` float(5, 2),
+ `weight` float (5,2),
+ `gender` Char(1) NOT NULL,
+ `birthdate` Date NOT NULL,
+ `biography` Text
+  );
+  
+insert into minions.people (`id`, `name`, `height`, `weight`, `gender`, `birthdate`, `biography`)
+values
+('1', 'Petar', '1.70', '72', 'M', '1980-01-21', 'Аз съм нов'),
+('2', 'Petar', '1.70', '72', 'M', '1980-02-22', 'Аз съм нов студент'),
+('3', 'Petar', '1.70', '72', 'M', '1980-01-23', 'Аз съм нов студент по'),
+('4', 'Petar', '1.70', '72', 'M', '1980-01-24', 'Аз съм нов студент по SQL'),
+('5', 'Petar', '1.70', '72', 'M', '1980-03-25', 'Аз съм нов студент в кърс по SQL');
+select * from people;
+
+
+Alter Table `users`
+DROP primary key,
+add constraint pk_users
+Primary key (`id`, `username`);
+CREATE TABLE users (
+user_email Varchar(50)
+);
+Create Database minions;
+ Create Table minions.people(
+ `id` INT Primary key auto_increment,
+ `name` VARCHAR (200) NOT NULL,
+ `picture` BLOB,
+ `height` float(5, 2),
+ `weight` float (5,2),
+ `gender` Char(1) NOT NULL,
+ `birthdate` Date NOT NULL,
+ `biography` Text
+  );
+INSERT INTO `minions`.`people` (`id`, `name`, `height`, `weight`, `gender`, `birthdate`, `biography`) VALUES ('1', 'Petar', '1.80', '78', 'M', '1981-11-01', 'aaa');
+INSERT INTO `minions`.`people` (`id`, `name`, `height`, `weight`, `gender`, `birthdate`, `biography`) VALUES ('2', 'Denka', '1.60', '53', 'F', '1981-11-02', 'sss');
+INSERT INTO `minions`.`people` (`id`, `name`, `height`, `weight`, `gender`, `birthdate`, `biography`) VALUES ('3', 'Genadi', '1.75', '68', 'M', '1981-11-03', 'ddd');
+INSERT INTO `minions`.`people` (`id`, `name`, `height`, `weight`, `gender`, `birthdate`, `biography`) VALUES ('4', 'Fitka', '1.78', '92', 'F', '1981-11-04', 'fff');
+INSERT INTO `minions`.`people` (`id`, `name`, `height`, `weight`, `gender`, `birthdate`, `biography`) VALUES ('5', 'Gligi', '1.88', '103', 'F', '1981-11-05', 'ggg');
+
+Create Table `users` (
+`id` INT Primary key auto_increment, 
+`username` Varchar(30),
+`password` Varchar(26),
+`profile_picture` BLOB,
+`last_login_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+`is_deleted` TINYINT(1) NOT NULL DEFAULT 0
+);
+insert into `users` (`username`, `password`)
+VALUES ('petar_d', '12345'),
+('gosho_d', '12345'),
+('dragan_d', '12345'),
+('dragan_d', '12345'),
+('strahil', '12345');
 
 -- Exercise 13 
  
@@ -70,7 +170,8 @@ VALUES
  Select * from `departments`;
  Select * from `employees`;
  
-  -- Exercise 15
+ 
+ -- Exercise 15
  
  Select * From `towns`
  order by `name` ;
@@ -89,11 +190,17 @@ Select `first_name`, `last_name`, `job_title`, `salary` from `employees`
 Order by `salary` desc;
 
 # Exercise 17
-
 SET SQL_SAFE_UPDATES = 1;
 UPDATE `employees`
 SET `salary` = `salary` * 1.1;
 -- WHERE `id` IS NOT NULL;
 Select `salary` from `employees`;
 Select * from `employees`;
+
+Delete from `employees`
+where `id` = 1;
+select * from `employees`;
+
+delete from `employees`
+where `id` = 3;
 
